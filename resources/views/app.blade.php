0a2adf5e7c75e547d8efeed2857520fd3e695c5e
Hordi Lusala <hordimboko@gmail.com>
2026-09-02 09:49:58 +0100

    fix(build): ajoute @viteReactRefresh, manquant depuis la mise en place initiale
    Sans cette directive avant @vite(...), le serveur de développement Vite
    (npm run dev) ne peut pas initialiser le rafraîchissement à chaud de
    React : la page reste blanche avec l'erreur console "@vitejs/plugin-react
    can't detect preamble". Passé inaperçu jusqu'ici car toute vérification
    manuelle précédente passait par des fichiers déjà construits (npm run
    build), qui n'empruntent pas ce chemin.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:ital,opsz,wght@0,6..96,400;0,6..96,500;0,6..96,600;0,6..96,700;1,6..96,400;1,6..96,500&family=Plus+Jakarta+Sans:wght@300;400;500;600;700&family=Space+Grotesk:wght@500;600;700&display=swap" rel="stylesheet">
+    @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/organizer/app.tsx'])
     @inertiaHead
 </head>
